9e0b54dfbc49f8d920dbc73a39465ef5bbd9c401
Olamide <onabiyiolamide13@gmail.com>
2025-01-28 20:11:44 +0100

    Remove ThemeProvider wrapper and clean up entry-server for improved structure

--- src/entry-server.ts
+++ src/entry-server.ts
@@ -1,53 +1,13 @@
-import type { AnyRouter } from "@tanstack/react-router";
-
 /// <reference types="vinxi/types/server" />
+
 import { getRouterManifest } from "@tanstack/start/router-manifest";
 import {
 	createStartHandler,
 	defaultStreamHandler,
 } from "@tanstack/start/server";
 
-import { createRouter } from "./router";
+import { createRouter } from "~/router";
 
-export type HandlerCallback<TRouter extends AnyRouter> = (ctx: {
-	request: Request;
-	router: TRouter;
-	responseHeaders: Headers;
-}) => Response | Promise<Response>;
-
-const streamHandler: HandlerCallback<AnyRouter> = (ctx) => {
-	ctx.router.serverSsr!.injectScript(
-		() => `function initTheme() {
-					if (typeof localStorage === 'undefined') return
-
-					const localTheme = localStorage.getItem('theme')
-					const preferTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-					const resolvedTheme = localTheme === null || localTheme === 'system' ? preferTheme : localTheme
-
-          console.log('initTheme', resolvedTheme)
-
-					if (localTheme === null) {
-						localStorage.setItem('theme', 'system')
-					}
-
-					const html = document.documentElement
-					const themes = ['light', 'dark']
-					for (const theme of themes) {
-						html.classList.remove(theme)
-					}
-
-					html.style.colorScheme = resolvedTheme
-					html.classList.add(resolvedTheme)
-					}
-
-					initTheme()`,
-	);
-	return defaultStreamHandler(ctx);
-};
-export default createStartHandler({
-	createRouter: () => {
-		const router = createRouter();
-		return router;
-	},
-	getRouterManifest,
-})(streamHandler);
+export default createStartHandler({ createRouter, getRouterManifest })(
+	defaultStreamHandler,
+);
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -96,7 +96,7 @@ function RootComponent() {
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
 	return (
-		<html lang="en" className="antialiased font-sans">
+		<html lang="en" className="antialiased font-sans dark">
 			<head>
 				<link rel="preconnect" href="https://rsms.me/" />
 				<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
