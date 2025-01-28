13befcc76c78cc66566333fb1bfb65049c497274
Olamide <onabiyiolamide13@gmail.com>
2025-01-28 19:08:43 +0100

    Yeeeeeeeeeeeeeeee

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -4,6 +4,7 @@ import {
 	useRouter,
 } from "@tanstack/react-router";
 import { RouterProvider } from "react-aria-components";
+import { ThemeProvider } from "./theme-provider";
 
 declare module "react-aria-components" {
 	interface RouterConfig {
@@ -20,9 +21,9 @@ export function Providers({ children }: { children: React.ReactNode }) {
 			navigate={(to, options) => router.navigate({ to, ...options })}
 			useHref={(to) => router.buildLocation({ to }).href}
 		>
-			{/* <ThemeProvider enableSystem attribute="class"> */}
-			{children}
-			{/* </ThemeProvider> */}
+			<ThemeProvider enableSystem attribute="class">
+				{children}
+			</ThemeProvider>
 		</RouterProvider>
 	);
 }
